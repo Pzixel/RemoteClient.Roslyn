af9d0520fc4803269a5d41d5d410f543c106303d
Psilon <pzixel@gmail.com>
2017-06-17 01:43:33 +0300

    Add project files.

---
+++ RemoteClient.Roslyn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoteClient.Roslyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
