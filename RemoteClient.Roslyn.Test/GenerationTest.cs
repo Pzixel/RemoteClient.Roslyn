86977a005d52b20a15b568e9debf742f0e599a50
Psilon <pzixel@gmail.com>
2017-11-07 17:27:45 +0300

    More tests

--- RemoteClient.Roslyn.Test/GenerationTest.cs
+++ RemoteClient.Roslyn.Test/GenerationTest.cs
@@ -10,7 +10,7 @@ namespace RemoteClient.Roslyn.Test
     public class GenerationTest
     {
         [Fact]
-        public async Task Test()
+        public async Task TestGet()
         {
             var mock = new Mock<IRemoteRequestProcessor>();
 
@@ -22,6 +22,33 @@ namespace RemoteClient.Roslyn.Test
             string result = await fooClient.GetStringAsync("10", "20");
 
             Assert.Equal("success", result);
+            Assert.NotNull(savedRequest);
+            Assert.Equal("GET", savedRequest.Descriptor.Method);
+            Assert.Equal("foo/{value}", savedRequest.Descriptor.UriTemplate);
+            Assert.Equal(OperationWebMessageFormat.Json, savedRequest.Descriptor.RequestFormat);
+            Assert.Equal(OperationWebMessageFormat.Xml, savedRequest.Descriptor.ResponseFormat);
+            Assert.Equal(1, savedRequest.QueryStringParameters.Count);
+            Assert.Equal(1, savedRequest.BodyParameters.Count);
+            var queryParameter = savedRequest.QueryStringParameters.Single();
+            var bodyParameter = savedRequest.BodyParameters.Single();
+            Assert.Equal("value", queryParameter.Key);
+            Assert.Equal("10", queryParameter.Value);
+            Assert.Equal("bar", bodyParameter.Key);
+            Assert.Equal("20", bodyParameter.Value);
+        }
+
+        [Fact]
+        public async Task TestExecute()
+        {
+            var mock = new Mock<IRemoteRequestProcessor>();
+
+            IRemoteRequest savedRequest = null;
+            mock.Setup(processor => processor.ExecuteAsync(It.IsAny<IRemoteRequest>()))
+                .Callback<IRemoteRequest>(request => savedRequest = request)
+                .Returns(Task.CompletedTask);
+			var fooClient = new FooClient(mock.Object);
+            await fooClient.ExecuteStringAsync("10", "20");
+
             Assert.NotNull(savedRequest);
             Assert.Equal("GET", savedRequest.Descriptor.Method);
             Assert.Equal("foo/{value}", savedRequest.Descriptor.UriTemplate);
